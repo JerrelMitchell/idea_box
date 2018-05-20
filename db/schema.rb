a7d78aa6775e2b8b179c8d6701ce899a417464ef
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-20 17:17:18 -0600

    add in ideas scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_212700) do
+ActiveRecord::Schema.define(version: 2018_05_20_214238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ideas", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2018_05_20_212700) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ideas", "users"
 end
