8597c4b3b63840af07bf42fd09d7dfadb06d924e
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-22 08:45:44 -0600

    remove password from user table, replace with password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_214238) do
+ActiveRecord::Schema.define(version: 2018_05_21_225456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 2018_05_20_214238) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "ideas", "users"
