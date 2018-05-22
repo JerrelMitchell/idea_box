a8bca05ddc2f3e07c92cdbc61efdadde3dc7c4d4
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-22 08:44:59 -0600

    update user spec with relationships and validations for user mode. add user registration feature test

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe 'visitor' do
+  context 'logging in' do
+    it 'should log in user' do
+      name = 'wow'
+      email = 'wow@gmail.com'
+      password = 'secret'
+      user = User.create!(name: name, email: email, password: password)
+
+      visit '/'
+      click_on 'Log In'
+
+      expect(current_path).to eq(login_path)
+
+      fill_in :email, with: email
+      fill_in :password, with: password
+      click_button 'Log In'
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_link('Log Out')
+    end
+  end
+
+  context 'fills out registration form' do
+    it 'should make a user account successfully' do
+      name = 'wow'
+      email = 'wow@gmail.com'
+
+      visit '/'
+
+      click_on 'Sign Up'
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in :user_name, with: name
+      fill_in :user_email, with: email
+      fill_in :user_password, with: 'supersecret'
+
+      click_on 'Create User'
+
+      expect(current_path).to eq(user_path(User.last))
+      expect(page).to have_content("Welcome, #{name}")
+    end
+
+    it 'should not allow duplicate user names' do
+      name = 'wow'
+      email = 'wow@gmail.com'
+      User.create!(name: name, email: email, password: 'secret')
+
+      visit new_user_path
+
+      fill_in :user_name, with: name
+      fill_in :user_email, with: email
+      fill_in :user_password, with: 'supersecret'
+
+      click_on 'Create User'
+
+      expect(current_path).to eq(users_path)
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
   end
 
   describe 'relationships' do
-    it { should have_many(:ideas)}
+    it { should have_many(:ideas) }
   end
 end
