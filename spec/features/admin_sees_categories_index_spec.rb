33612956809ee1732011eb954d098efebe9eeb1d
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-23 13:28:21 -0600

    update categories for admin users. show page, index page, and create

--- spec/features/admin_sees_categories_index_spec.rb
+++ spec/features/admin_sees_categories_index_spec.rb
@@ -9,20 +9,7 @@ describe 'user visits categories index page' do
 
       visit admin_categories_path
 
-      expect(page).to have_content('Admin Categories')
-    end
-  end
-
-  context 'as default user' do
-    it 'does not allow me to see admin categories index' do
-      user = User.create(name: 'Bob', email: 'bob@example.com', password: 'secret1233', role: 0)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      visit admin_categories_path
-
-      expect(page).to_not have_content('Admin Categories')
-      expect(page).to have_content("The page you were looking for doesn't exist")
+      expect(page).to have_content('Categories')
     end
   end
 end
