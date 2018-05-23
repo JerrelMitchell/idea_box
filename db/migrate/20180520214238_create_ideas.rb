78b5b1cd8ff9bdc3c53ebb87097d6b7f706f2db8
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-22 22:12:17 -0600

    add in categories table. update ideas table to have title, user reference , and category reference. update user table for role column

--- db/migrate/20180520214238_create_ideas.rb
+++ db/migrate/20180520214238_create_ideas.rb
@@ -2,7 +2,6 @@ class CreateIdeas < ActiveRecord::Migration[5.2]
   def change
     create_table :ideas do |t|
       t.text :content
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_225456) do
+ActiveRecord::Schema.define(version: 2018_05_22_222530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ideas", force: :cascade do |t|
-    t.text "content"
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.string "title"
+    t.bigint "user_id"
+    t.index ["category_id"], name: "index_ideas_on_category_id"
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
@@ -29,7 +40,10 @@ ActiveRecord::Schema.define(version: 2018_05_21_225456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "ideas", "categories"
   add_foreign_key "ideas", "users"
 end
