632f3a816af6b1581cd2659f886a9dc4abf66e98
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-23 23:03:01 -0600

    add user account path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get 'users/:id/account', to: 'users#show', as: :user_account
+
 
   root to: 'sessions#new'
 
